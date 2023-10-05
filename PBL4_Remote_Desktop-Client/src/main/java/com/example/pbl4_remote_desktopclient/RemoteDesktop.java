03325b043574867caea604f092055619d5c77d5e
PenguinBKDN <son72ltv2@gmail.com>
2023-10-05 23:49:11 +0700

    again

--- PBL4_Remote_Desktop-Client/src/main/java/com/example/pbl4_remote_desktopclient/RemoteDesktop.java
+++ PBL4_Remote_Desktop-Client/src/main/java/com/example/pbl4_remote_desktopclient/RemoteDesktop.java
@@ -1,20 +1,77 @@
 package com.example.pbl4_remote_desktopclient;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Random;
 
 public class RemoteDesktop  {
+    @FXML
+    private TextField localIp;
 
     @FXML
-    private Button btnConnect;
+    private TextField localPwd;
 
     @FXML
-    private RadioButton radioRemoteDesktop;
+    private TextField remoteIp;
 
     @FXML
-    private RadioButton radioTransferFile;
+    private PasswordField remotePwd;
 
+    Socket socketClient = null;
+
+    public void setSocketClient(Socket socketClient) {
+        this.socketClient = socketClient;
+    }
+
+    public void setValue(String pwd)
+    {
+        try {
+            // Lấy địa chỉ IP của máy
+            String ipAddress = InetAddress.getLocalHost().getHostAddress();
+
+            // Đặt giá trị của tfYourID bằng địa chỉ IP
+            localIp.setText(ipAddress);
+
+            //Password default
+            localPwd.setText(pwd);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    public void connectBtn(MouseEvent mouseEvent) {
+        String ip = "", pwd = "";
+        try {
+            ip = remoteIp.getText();
+            pwd = remotePwd.getText();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        if (!ip.equals("") && !pwd.equals("")) {
+            Authentication auth = new Authentication(socketClient, ip, pwd);
+            auth.validation();
+        }
+    }
 }
 
--- PBL4_Remote_Desktop-Client/src/main/java/com/example/pbl4_remote_desktopclient/StartWindow.java
+++ PBL4_Remote_Desktop-Client/src/main/java/com/example/pbl4_remote_desktopclient/StartWindow.java
@@ -1,49 +1,65 @@
 package com.example.pbl4_remote_desktopclient;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.Socket;
 
 public class StartWindow extends Application {
-
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(StartWindow.class.getResource("StartWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.initStyle(StageStyle.UNDECORATED);
-        stage.setScene(scene);
-        stage.show();
-//        String serverName = "192.168.1.14";
-//        int port = Integer.parseInt("6003");
-//
-//        try {
-//            System.out.println("Connecting to " + serverName
-//                    + " on port " + port);
-//            Socket client = new Socket(serverName, port);
-//
-//            System.out.println("Just connected to "
-//                    + client.getRemoteSocketAddress());
-//            OutputStream outToServer = client.getOutputStream();
-//            DataOutputStream out = new DataOutputStream(outToServer);
-//
-//            out.writeUTF("Hello from " + client.getLocalSocketAddress());
-//            InputStream inFromServer = client.getInputStream();
-//            DataInputStream in = new DataInputStream(inFromServer);
-//
-//            System.out.println("Server says " + in.readUTF());
-//            client.close();
-//        }catch (IOException e) {
-//            e.printStackTrace();
-//        }
+    public void start(Stage stage) throws IOException {
+        String ip = "192.168.1.14";
+        int port = 6003;
+        int maxAttempts = 10; // Maximum number of connection attempts
+
+        boolean connected = false;
+        Socket client = null;
+
+        // Attempt to connect to the server with retries
+        for (int attempt = 1; attempt <= maxAttempts; attempt++) {
+            try {
+                client = new Socket(ip, port);
+                System.out.println("Connected...");
+                connected = true;
+                break; // Connection successful, exit the loop
+            } catch (IOException e) {
+                System.out.println("Connection attempt " + attempt + " failed. Retrying...");
+                try {
+                    Thread.sleep(1000); // Wait for 1 second before retrying
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+
+        if (connected) {
+            FXMLLoader fxmlLoader = new FXMLLoader(StartWindow.class.getResource("StartWindow.fxml"));
+            Pane pane = fxmlLoader.load();
+
+            StartWindowController controller = fxmlLoader.getController();
+            controller.setClientSocket(client);
+
+            Scene scene = new Scene(pane);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            System.out.println("Failed to connect to the server after " + maxAttempts + " attempts.");
+        }
     }
 }
